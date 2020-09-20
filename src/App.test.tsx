233be014e2ffab5b8e1a99b3c8397d183a7c3bf3
Moreno Andrade <laforetmoreno@gmail.com>
2020-09-20 16:07:13 -0300

    Add auth usecase

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders app', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/app/i);
-  expect(linkElement).toBeInTheDocument();
-});
